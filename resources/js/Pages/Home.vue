db63861b5ed401fafc9ff01e78ab2ca8a175daf2
Chris Dawson <csdawson8@gmail.com>
2022-05-17 19:08:01 +0100

    Pages

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>Hello {{ name }}</h1>
+
+    <ul>
+        <li v-for="framework of frameworks" v-text="framework"></li>
+    </ul>
+</template>
+
+<script>
+
+export default {
+    props: {
+        name: String,
+        frameworks: Array,
+    },
+};
+
+</script>
+
