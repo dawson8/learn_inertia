f04d57b95ce03d5ba3599e25485e457c6500ec82
dawson8 <csdawson8@gmail.com>
2022-05-18 15:16:03 +0100

    Inertia's Form Helper

--- resources/js/Pages/Users/Create.vue
+++ resources/js/Pages/Users/Create.vue
@@ -13,7 +13,7 @@
                 id="name"
                 required
             >
-            <div v-if="errors.name" v-text="errors.name" class="text-red-500 text-xs mt-1"></div>
+            <div v-if="form.errors.name" v-text="form.errors.name" class="text-red-500 text-xs mt-1"></div>
         </div>
         <div class="mb-6">
             <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="email">
@@ -25,7 +25,7 @@
                 id="email"
                 required
             >
-            <div v-if="errors.email" v-text="errors.email" class="text-red-500 text-xs mt-1"></div>
+            <div v-if="form.errors.email" v-text="form.errors.email" class="text-red-500 text-xs mt-1"></div>
         </div>
         <div class="mb-6">
             <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="password">
@@ -37,10 +37,10 @@
                 id="password"
                 required
             >
-            <div v-if="errors.password" v-text="errors.password" class="text-red-500 text-xs mt-1"></div>
+            <div v-if="form.errors.password" v-text="form.errors.password" class="text-red-500 text-xs mt-1"></div>
         </div>
         <div class="mb-6">
-            <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">
+            <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500" :disabled="form.processing">
                 Submit
             </button>
         </div>
@@ -48,20 +48,15 @@
 </template>
 
 <script setup>
-    import { reactive } from "vue";
-    import { Inertia } from "@inertiajs/inertia";
+    import { useForm } from "@inertiajs/inertia-vue3"
 
-    defineProps({
-        errors: Object
-    });
-
-    let form = reactive({
+    let form = useForm({
         name: '',
         email: '',
         password: ''
     });
 
     let submit = () => {
-        Inertia.post('/users', form);
+        form.post('/users');
     };
 </script>
